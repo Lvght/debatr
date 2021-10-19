389aaf6903480c6b4f40de2dd47b8a7638aa00a9
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2021-10-19 16:52:53 -0300

    Initial commit
    Co-authored-by: lucas-cid <lucas_mcid@hotmail.com>
    Co-authored-by: AndreLRoJr <andre.rodrigues@estudante.ufscar.br>
    Co-authored-by: NataliaBachiega <nataliabachiega@hotmail.com>

---
+++ src/main/java/br/ufscar/dc/dsw1/debatr/DebatrApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw1.debatr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebatrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebatrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufscar/dc/dsw1/debatr/DebatrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw1.debatr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebatrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
