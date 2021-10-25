827c4d25820674b6142103cdc331b8d5860fd2f7
lucas-cid <lucas_mcid@hotmail.com>
2021-10-25 15:48:40 -0300

    Home layout creation

--- src/main/java/br/ufscar/dc/dsw1/debatr/controller/TestController.java
+++ src/main/java/br/ufscar/dc/dsw1/debatr/controller/TestController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TestController {
     @GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/")
+    public String home(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "home";
     }
 }
--- src/main/java/br/ufscar/dc/dsw1/debatr/dao/IPostDAO.java
+++ src/main/java/br/ufscar/dc/dsw1/debatr/dao/IPostDAO.java
@@ -11,17 +11,19 @@ import br.ufscar.dc.dsw1.debatr.domain.Post;
 @SuppressWarnings("unchecked")
 public interface IPostDAO extends CrudRepository<Post, Long> {
 
-	Post findById(long id);
+	// Post findById(long id);
 
-	List<Post> findAll();
+	// List<Post> findAll();
 
-	Post save(Post post);
+	// Post save(Post post);
 
-	void deleteById(Long id);
+	// void deleteById(Long id);
 
-	@Query("WHERE id_forum = :id_forum AND id_topico = :id_topico get_forum_posts_1 get_forum_posts_2")
-	public List<Post> getForumPosts(@Param("id_forum") Long id_forum, @Param("id_topico") Long id_topico);
+	// @Query("WHERE id_forum = :id_forum AND id_topico = :id_topico
+	// get_forum_posts_1 get_forum_posts_2")
+	// public List<Post> getForumPosts(@Param("id_forum") Long id_forum,
+	// @Param("id_topico") Long id_topico);
 
-	@Query("SELECT COUNT(*) from postagem WHERE id_forum = :id_forum")
-	public Long countForumPosts(@Param("id_forum") Long id_forum);
+	// @Query("SELECT COUNT(*) from postagem WHERE id_forum = :id_forum")
+	// public Long countForumPosts(@Param("id_forum") Long id_forum);
 }
