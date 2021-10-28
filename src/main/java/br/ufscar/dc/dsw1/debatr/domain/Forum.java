268f64f3a46c8b7c9bb5d7c869c79cd89493eeae
lucas-cid <lucas_mcid@hotmail.com>
2021-10-28 13:32:45 -0300

    Forum creation and list implementation

--- src/main/java/br/ufscar/dc/dsw1/debatr/domain/Forum.java
+++ src/main/java/br/ufscar/dc/dsw1/debatr/domain/Forum.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Table(name = "forum")
 public class Forum extends AbstractEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne()
