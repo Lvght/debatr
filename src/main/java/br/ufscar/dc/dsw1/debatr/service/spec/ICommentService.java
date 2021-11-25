1f3c9dfc120a25b6c7e503c5cd4f7df92d76903e
AndreLRoJr <89857946+AndreLRoJr@users.noreply.github.com>
2021-11-25 12:25:40 -0300

    Add files via upload

--- src/main/java/br/ufscar/dc/dsw1/debatr/service/spec/ICommentService.java
+++ src/main/java/br/ufscar/dc/dsw1/debatr/service/spec/ICommentService.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import br.ufscar.dc.dsw1.debatr.domain.Comment;
 import br.ufscar.dc.dsw1.debatr.domain.Post;
+import br.ufscar.dc.dsw1.debatr.domain.User;
 
 public interface ICommentService {
     Comment findById(long id);
+    
+    List<Comment> findAll();
 
     List<Comment> buscarPorPost(Post post);
-
-    void save(Comment comment);
+    
+    Comment save(Comment comment);
 
     void delete(Comment comment);
 }
